89d9cee72d4ccdf576443d8d66fbd471259fcc64
Vaclav Macha <vaclav.macha92@gmail.com>
2024-01-28 12:12:10 +0100

    Refactor test cases to use to_numpy() function

--- test/test_utilities.py
+++ test/test_utilities.py
@@ -37,29 +37,66 @@ def test_to_torch(device, x, expected):
 @pytest.mark.parametrize(
     "x, k, reverse, expected",
     [
-        *[(np.array(range(1, 11)), k, False, (k + 1, k)) for k in range(10)],
-        *[(np.array(range(1, 11)), k, True, (10 - k, 9 - k)) for k in range(10)],
+        (range(1, 11), 0, False, (1, 0)),
+        (range(1, 11), 1, False, (2, 1)),
+        (range(1, 11), 2, False, (3, 2)),
+        (range(1, 11), 3, False, (4, 3)),
+        (range(1, 11), 4, False, (5, 4)),
+        (range(1, 11), 5, False, (6, 5)),
+        (range(1, 11), 6, False, (7, 6)),
+        (range(1, 11), 7, False, (8, 7)),
+        (range(1, 11), 8, False, (9, 8)),
+        (range(1, 11), 9, False, (10, 9)),
+        (range(1, 11), 0, True, (10, 9)),
+        (range(1, 11), 1, True, (9, 8)),
+        (range(1, 11), 2, True, (8, 7)),
+        (range(1, 11), 3, True, (7, 6)),
+        (range(1, 11), 4, True, (6, 5)),
+        (range(1, 11), 5, True, (5, 4)),
+        (range(1, 11), 6, True, (4, 3)),
+        (range(1, 11), 7, True, (3, 2)),
+        (range(1, 11), 8, True, (2, 1)),
+        (range(1, 11), 9, True, (1, 0)),
     ],
 )
 def test_find_kth(x, k, reverse, expected):
-    actual = find_kth(x, k, reverse)
+    actual = find_kth(to_numpy(x), k, reverse)
     assert actual == expected
 
 
 @pytest.mark.parametrize(
     "x, q, top, expected",
     [
-        *[(np.array(range(1, 12)), k / 10, False, (k + 1, k)) for k in range(11)],
-        *[(np.array(range(1, 12)), k / 10, True, (11 - k, 10 - k)) for k in range(11)],
+        (range(1, 12), 0.0, False, (1, 0)),
+        (range(1, 12), 0.1, False, (2, 1)),
+        (range(1, 12), 0.2, False, (3, 2)),
+        (range(1, 12), 0.3, False, (4, 3)),
+        (range(1, 12), 0.4, False, (5, 4)),
+        (range(1, 12), 0.5, False, (6, 5)),
+        (range(1, 12), 0.6, False, (7, 6)),
+        (range(1, 12), 0.7, False, (8, 7)),
+        (range(1, 12), 0.8, False, (9, 8)),
+        (range(1, 12), 0.9, False, (10, 9)),
+        (range(1, 12), 1.0, False, (11, 10)),
+        (range(1, 12), 0.0, True, (11, 10)),
+        (range(1, 12), 0.1, True, (10, 9)),
+        (range(1, 12), 0.2, True, (9, 8)),
+        (range(1, 12), 0.3, True, (8, 7)),
+        (range(1, 12), 0.4, True, (7, 6)),
+        (range(1, 12), 0.5, True, (6, 5)),
+        (range(1, 12), 0.6, True, (5, 4)),
+        (range(1, 12), 0.7, True, (4, 3)),
+        (range(1, 12), 0.8, True, (3, 2)),
+        (range(1, 12), 0.9, True, (2, 1)),
     ],
 )
 class TestFindQuantile:
     def test_against_expected(self, x, q, top, expected):
-        actual = find_quantile(x, q, top)
+        actual = find_quantile(to_numpy(x), q, top)
         assert actual == expected
 
     def test_against_numpy(self, x, q, top, expected):
-        actual = find_quantile(x, q, top)
+        actual = find_quantile(to_numpy(x), q, top)
         if top:
             expected = np.quantile(x, 1 - q, interpolation="nearest")
         else:
