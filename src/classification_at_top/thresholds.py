b467a2c3b16908ea276a7daa10aa9c767525d145
Vaclav Macha <vaclav.macha92@gmail.com>
2024-01-28 19:17:09 +0100

    Add classification threshold classes

---
+++ src/classification_at_top/thresholds.py
@@ -0,0 +1,190 @@
+from abc import ABC, abstractmethod
+from typing import Tuple
+
+import attrs.converters as converters
+import attrs.validators as validators
+import numpy as np
+from attrs import define, field
+
+from .selectors import AbstractSelector, All
+from .utilities import (
+    ArrayLike,
+    Number,
+    find_kth,
+    find_maximum,
+    find_minimum,
+    find_quantile,
+    to_numpy,
+)
+
+
+class AbstractThreshold(ABC):
+    """
+    Abstract class representing a classification threshold.
+    """
+
+    @abstractmethod
+    def find_threshold(self, y: ArrayLike, s: ArrayLike) -> Tuple[Number, int]:
+        """
+        Finds the threshold and its index in the given array of scores `s`. This method must be implemented by subclasses.
+
+        Args:
+            y (ArrayLike): The target values.
+            s (ArrayLike): The score values.
+
+        Returns:
+            Tuple[Number, int]: A tuple containing the threshold and its index.
+        """
+        pass
+
+
+@define(frozen=True)
+class Maximum(AbstractThreshold):
+    """
+    A class representing a classification threshold equal to the maximum sample.
+
+    Attributes:
+        selector (AbstractSelector): The selector that decides whether the threshold is
+            computed from all, positive, or negative samples.
+    """
+
+    selector: AbstractSelector = field(
+        default=All(),
+        validator=validators.instance_of(AbstractSelector),
+    )
+
+    def find_threshold(self, y: ArrayLike, s: ArrayLike) -> Tuple[Number, int]:
+        """
+        Finds the maximum and its index from in given array of scores `s`.
+        Only scores selected by the selector are considered.
+
+        Args:
+            y (ArrayLike): The target values.
+            s (ArrayLike): The score values.
+
+        Returns:
+            Tuple[Number, int]: A tuple containing the threshold and its index.
+        """
+        s_selected, inds = self.selector.select(to_numpy(y), to_numpy(s))
+        t, t_ind = find_maximum(s_selected)
+
+        return t, inds[t_ind]
+
+
+@define(frozen=True)
+class Minimum(AbstractThreshold):
+    """
+    A class representing a classification threshold equal to the minimum sample.
+
+    Attributes:
+        selector (AbstractSelector): The selector that decides whether the threshold is
+            computed from all, positive, or negative samples.
+    """
+
+    selector: AbstractSelector = field(
+        default=All(),
+        validator=validators.instance_of(AbstractSelector),
+    )
+
+    def find_threshold(self, y: ArrayLike, s: ArrayLike) -> Tuple[Number, int]:
+        """
+        Finds the minimum and its index in the given array of scores `s`.
+        Only scores selected by the selector are considered.
+
+        Args:
+            y (ArrayLike): The target values.
+            s (ArrayLike): The score values.
+
+        Returns:
+            Tuple[Number, int]: A tuple containing the threshold and its index.
+        """
+        s_selected, inds = self.selector.select(to_numpy(y), to_numpy(s))
+        t, t_ind = find_minimum(s_selected)
+
+        return t, inds[t_ind]
+
+
+@define(frozen=True)
+class Kth(AbstractThreshold):
+    """
+    A class representing a classification threshold equal to the k-th largest/smallest
+    sample.
+
+    Attributes:
+        k (int): The index of the element to find in the sorted array.
+        reverse (bool): If True, the array is sorted in descending order. If False, the
+            array is sorted in ascending order. Defaults to True.
+        selector (AbstractSelector): The selector that decides whether the threshold is
+            computed from all, positive, or negative samples.
+    """
+
+    k: int = field(
+        converter=int,
+        validator=validators.ge(0),
+    )
+    reverse: bool = field(
+        default=True,
+        converter=converters.to_bool,
+    )
+    selector: AbstractSelector = field(
+        default=All(),
+        validator=validators.instance_of(AbstractSelector),
+    )
+
+    def find_threshold(self, y: ArrayLike, s: ArrayLike) -> Tuple[Number, int]:
+        """
+        Finds the k-th largest/smallest sample and its index from in given array of scores
+        `s`. Only scores selected by the selector are considered.
+
+        Args:
+            y (ArrayLike): The target values.
+            s (ArrayLike): The score values.
+
+        Returns:
+            Tuple[Number, int]: A tuple containing the threshold and its index.
+        """
+        s_selected, inds = self.selector.select(to_numpy(y), to_numpy(s))
+        t, t_ind = find_kth(s_selected, self.k, self.reverse)
+        return t, inds[t_ind]
+
+
+@define(frozen=True)
+class Quantile(AbstractThreshold):
+    """
+    A class representing a classification threshold equal to the top/bottom quantile.
+
+    Attributes:
+        tau (float): The quantile value to use for thresholding. Must be between 0 and 1.
+        top (bool): If True, finds the quantile from the top of the array. If False, finds
+            the quantile from the bottom. Defaults to False.
+        selector (AbstractSelector): The selector used to select the data for thresholding.
+    """
+
+    tau: float = field(
+        converter=float,
+        validator=[validators.ge(0), validators.le(1)],
+    )
+    top: bool = field(
+        default=True,
+        converter=converters.to_bool,
+    )
+    selector: AbstractSelector = field(
+        default=All(),
+        validator=validators.instance_of(AbstractSelector),
+    )
+
+    def find_threshold(self, y: ArrayLike, s: ArrayLike) -> Tuple[Number, int]:
+        """
+        Finds the top/bottom tau-quantile and its index from in given array of scores
+        `s`. Only scores selected by the selector are considered.
+
+        Args:
+            y (ArrayLike): The target values.
+            s (ArrayLike): The score values.
+
+        Returns:
+            Tuple[Number, int]: A tuple containing the threshold and its index
+        """
+        s_selected, inds = self.selector.select(to_numpy(y), to_numpy(s))
+        t, t_ind = find_quantile(s_selected, self.tau, self.top)
+        return t, inds[t_ind]
---
+++ test/test_thresholds.py
@@ -0,0 +1,173 @@
+import pytest
+from classification_at_top.selectors import All, Negatives, Positives
+from classification_at_top.thresholds import Kth, Maximum, Minimum, Quantile
+from classification_at_top.utilities import (
+    find_kth,
+    find_maximum,
+    find_minimum,
+    find_quantile,
+    to_numpy,
+)
+
+
+@pytest.fixture
+def scores_dict() -> dict:
+    return {
+        "labels": [1, 1, 0, 1, 0, 1, 0, 0, 1, 0],
+        "scores": list(range(1, 11)),
+        "scores_neg": [3, 5, 7, 8, 10],
+        "scores_pos": [1, 2, 4, 6, 9],
+    }
+
+
+@pytest.mark.parametrize(
+    "selector, key, expected",
+    [
+        (All(), "scores", (10, 9)),
+        (Negatives(), "scores_neg", (10, 9)),
+        (Negatives(0), "scores_neg", (10, 9)),
+        (Negatives(1), "scores_pos", (9, 8)),
+        (Positives(), "scores_pos", (9, 8)),
+        (Positives(1), "scores_pos", (9, 8)),
+        (Positives(0), "scores_neg", (10, 9)),
+    ],
+)
+class TestMaximum:
+    def test_expected(self, selector, key, expected, request):
+        fix = request.getfixturevalue("scores_dict")
+        threshold = Maximum(selector=selector)
+
+        actual = threshold.find_threshold(fix["labels"], fix["scores"])
+        assert actual == expected
+
+    def test_value(self, selector, key, expected, request):
+        fix = request.getfixturevalue("scores_dict")
+        threshold = Maximum(selector=selector)
+
+        actual = threshold.find_threshold(fix["labels"], fix["scores"])
+        assert actual[0] == find_maximum(to_numpy(fix[key]))[0]
+
+    def test_kth(self, selector, key, expected, request):
+        fix = request.getfixturevalue("scores_dict")
+        threshold1 = Maximum(selector=selector)
+        threshold2 = Kth(k=0, reverse=True, selector=selector)
+
+        t1 = threshold1.find_threshold(fix["labels"], fix["scores"])
+        t2 = threshold2.find_threshold(fix["labels"], fix["scores"])
+        assert t1 == t2
+
+
+@pytest.mark.parametrize(
+    "selector, key, expected",
+    [
+        (All(), "scores", (1, 0)),
+        (Negatives(), "scores_neg", (3, 2)),
+        (Negatives(0), "scores_neg", (3, 2)),
+        (Negatives(1), "scores_pos", (1, 0)),
+        (Positives(), "scores_pos", (1, 0)),
+        (Positives(1), "scores_pos", (1, 0)),
+        (Positives(0), "scores_neg", (3, 2)),
+    ],
+)
+class TestMinimum:
+    def test_expected(self, selector, key, expected, request):
+        fix = request.getfixturevalue("scores_dict")
+        threshold = Minimum(selector=selector)
+
+        actual = threshold.find_threshold(fix["labels"], fix["scores"])
+        assert actual == expected
+
+    def test_value(self, selector, key, expected, request):
+        fix = request.getfixturevalue("scores_dict")
+        threshold = Minimum(selector=selector)
+
+        actual = threshold.find_threshold(fix["labels"], fix["scores"])
+        assert actual[0] == find_minimum(to_numpy(fix[key]))[0]
+
+    def test_kth(self, selector, key, expected, request):
+        fix = request.getfixturevalue("scores_dict")
+        threshold1 = Minimum(selector=selector)
+        threshold2 = Kth(k=0, reverse=False, selector=selector)
+
+        t1 = threshold1.find_threshold(fix["labels"], fix["scores"])
+        t2 = threshold2.find_threshold(fix["labels"], fix["scores"])
+        assert t1 == t2
+
+
+@pytest.mark.parametrize(
+    "selector, k, rev, key, expected",
+    [
+        (All(), 1, False, "scores", (2, 1)),
+        (Negatives(), 1, False, "scores_neg", (5, 4)),
+        (Negatives(0), 1, False, "scores_neg", (5, 4)),
+        (Negatives(1), 1, False, "scores_pos", (2, 1)),
+        (Positives(), 1, False, "scores_pos", (2, 1)),
+        (Positives(1), 1, False, "scores_pos", (2, 1)),
+        (Positives(0), 1, False, "scores_neg", (5, 4)),
+        (All(), 1, True, "scores", (9, 8)),
+        (Negatives(), 1, True, "scores_neg", (8, 7)),
+        (Negatives(0), 1, True, "scores_neg", (8, 7)),
+        (Negatives(1), 1, True, "scores_pos", (6, 5)),
+        (Positives(), 1, True, "scores_pos", (6, 5)),
+        (Positives(1), 1, True, "scores_pos", (6, 5)),
+        (Positives(0), 1, True, "scores_neg", (8, 7)),
+    ],
+)
+class TestKth:
+    def test_expected(self, selector, k, rev, key, expected, request):
+        fix = request.getfixturevalue("scores_dict")
+        threshold = Kth(k=k, reverse=rev, selector=selector)
+
+        actual = threshold.find_threshold(fix["labels"], fix["scores"])
+        assert actual == expected
+
+    def test_value(self, selector, k, rev, key, expected, request):
+        fix = request.getfixturevalue("scores_dict")
+        threshold = Kth(k=k, reverse=rev, selector=selector)
+
+        actual = threshold.find_threshold(fix["labels"], fix["scores"])
+        assert actual[0] == find_kth(to_numpy(fix[key]), k, rev)[0]
+
+
+@pytest.mark.parametrize(
+    "selector, tau, top, key, expected",
+    [
+        (All(), 0.8, False, "scores", (8, 7)),
+        (Negatives(), 0.8, False, "scores_neg", (8, 7)),
+        (Negatives(0), 0.8, False, "scores_neg", (8, 7)),
+        (Negatives(1), 0.8, False, "scores_pos", (6, 5)),
+        (Positives(), 0.8, False, "scores_pos", (6, 5)),
+        (Positives(1), 0.8, False, "scores_pos", (6, 5)),
+        (Positives(0), 0.8, False, "scores_neg", (8, 7)),
+        (All(), 0.2, True, "scores", (9, 8)),
+        (Negatives(), 0.2, True, "scores_neg", (10, 9)),
+        (Negatives(0), 0.2, True, "scores_neg", (10, 9)),
+        (Negatives(1), 0.2, True, "scores_pos", (9, 8)),
+        (Positives(), 0.2, True, "scores_pos", (9, 8)),
+        (Positives(1), 0.2, True, "scores_pos", (9, 8)),
+        (Positives(0), 0.2, True, "scores_neg", (10, 9)),
+    ],
+)
+class TestQuantile:
+    def test_expected(self, selector, tau, top, key, expected, request):
+        fix = request.getfixturevalue("scores_dict")
+        threshold = Quantile(tau=tau, top=top, selector=selector)
+
+        actual = threshold.find_threshold(fix["labels"], fix["scores"])
+        assert actual == expected
+
+    def test_value(self, selector, tau, top, key, expected, request):
+        fix = request.getfixturevalue("scores_dict")
+        threshold = Quantile(tau=tau, top=top, selector=selector)
+
+        actual = threshold.find_threshold(fix["labels"], fix["scores"])
+        assert actual[0] == find_quantile(to_numpy(fix[key]), tau, top)[0]
+
+    def test_negation(self, selector, tau, top, key, expected, request):
+        fix = request.getfixturevalue("scores_dict")
+        threshold1 = Quantile(tau=tau, top=top, selector=selector)
+        threshold2 = Quantile(tau=1 - tau, top=not top, selector=selector)
+
+        t1 = threshold1.find_threshold(fix["labels"], fix["scores"])
+        t2 = threshold2.find_threshold(fix["labels"], fix["scores"])
+        assert t1 == t2
